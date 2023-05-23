f45ef53403d5ae53646ca542559b8a26fa55e840
Juan Mercado <juan.mercado1986@gmail.com>
2023-05-23 12:44:49 -0300

    ADD refactorizacion de funciones

--- semana-1/clase1.js
+++ semana-1/clase1.js
@@ -1,57 +1,39 @@
 /* -------------------------------------------------------------------------- */
 /*                                  FUNCION 1                                 */
 /* -------------------------------------------------------------------------- */
-// function iniciarJuego() {
-//     // Saludo al usuario
-//     alert("Bienvenido al popular juego de piedra papel o tijera de Frontend 2.");
-//     // Guardar en una variable el nombre ingresado. 
-//     const nombre = prompt("Ingrese su nombre por favor: ");
-
-//     // alert("Gracias por jugar " + nombre + ". ¡Mucha suerte!");
-//     alert(`Gracias por jugar ${nombre}. ¡Mucha suerte!`);
-
-//     console.log("---------------------");
-//     console.log("El nombre del jugador es: ");
-//     console.log(nombre);
-//     console.log("---------------------");
+function iniciarJuego() {
+    alert("Bienvenido al popular juego de piedra papel o tijera de Frontend 2.");
+    const nombre = obtenerNombreValido();
+    alert(`Gracias por jugar ${nombre}. ¡Mucha suerte!`);
     
-//     return nombre
-// }
+    return nombre
+}
 
-// // creamos una variable a nivel global para guardar el nombre del jugador que nos devuelve la función
-// const nombreJugador = iniciarJuego();
+function obtenerNombreValido() {
+    let nombre = "";
 
+    while(true){
+        nombre = prompt("Ingrese su nombre por favor: ");
+        if(esValido(nombre)) break;
+        alert("Debe ingresar un nombre valido (Al menos 3 caracteres y sin numeros)");
+    }
+
+    return nombre.toUpperCase();
+}
+
+function esValido(valorIngresado) {
+    // if (valorIngresado.length < 3 || valorIngresado.isEmpty == "" || valorIngresado.isNull) return false;
+    // for (let i = 0; i < valorIngresado.length; i++) {
+    //     if (!isNaN(valorIngresado[i])) return false;
+    // }
+    // return true;
+    
+    // Version Simplificada
+    return (valorIngresado === null || valorIngresado.length < 3 || /\d/.test(valorIngresado)) ? false : true;
+}
 /* -------------------------------------------------------------------------- */
 /*                          CONSIGNA MESA DE TRABAJO                          */
 /* -------------------------------------------------------------------------- */
 // 1- Modificar la funcion de iniciarJuego(), validar si ingresa un dato válido como nombre.
 // 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
 // 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
-
-
-function iniciarJuego() {
-    let nombre = "";
-    // Saludo al usuario
-    alert("Bienvenido al popular juego de piedra papel o tijera de Frontend 2.");
-    // Guardar en una variable el nombre ingresado. 
-
-    do {
-        nombre = prompt("Ingrese su nombre por favor: ");        
-    } while (!isNaN(nombre) && nombre.length >= 3);
-
-    nombre = nombre.toUpperCase()
-
-    // alert("Gracias por jugar " + nombre + ". ¡Mucha suerte!");
-    alert(`Gracias por jugar ${nombre}. ¡Mucha suerte!`);
-
-    console.log("---------------------");
-    console.log("El nombre del jugador es: ");
-    console.log(nombre);
-    console.log("---------------------");
-    
-    return nombre
-}
-// main()
-
-// creamos una variable a nivel global para guardar el nombre del jugador que nos devuelve la función
-// const nombreJugador = iniciarJuego();
